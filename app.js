522ccaa69dfce51f68e62084d782ddba3ad72318
Leo B Jacob <leobjacob@gmail.com>
2021-12-11 22:53:03 -0500

    push to saasbook

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const session = require("express-session");
-var pjax = require("express-pjax");
-const constructorMethod = require("./routeConstructor");
-require("dotenv").config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+var pjax = require('express-pjax');
+const constructorMethod = require('./routeConstructor');
+require('dotenv').config();
 
 const handlebarsInstance = exphbs.create({
-    defaultLayout: "main",
-    // Specify helpers which are only registered on this instance.
-    helpers: {
-        asJSON: (obj, spacing) => {
-            if (typeof spacing === "number")
-                return new Handlebars.SafeString(
-                    JSON.stringify(obj, null, spacing)
-                );
+	defaultLayout: 'main',
+	// Specify helpers which are only registered on this instance.
+	helpers: {
+		asJSON: (obj, spacing) => {
+			if (typeof spacing === 'number')
+				return new Handlebars.SafeString(
+					JSON.stringify(obj, null, spacing)
+				);
 
-            return new Handlebars.SafeString(JSON.stringify(obj));
-        },
-    },
-    partialsDir: ["views/partials/"],
+			return new Handlebars.SafeString(JSON.stringify(obj));
+		},
+	},
+	partialsDir: ['views/partials/'],
 });
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(pjax());
 app.use(
-    session({
-        name: "AuthCookie",
-        secret: "some secret string!",
-        resave: false,
-        saveUninitialized: false,
-    })
+	session({
+		name: 'AuthCookie',
+		secret: 'some secret string!',
+		resave: false,
+		saveUninitialized: false,
+	})
 );
 
-const static = express.static(__dirname + "/public");
-app.use("/public", static);
-app.engine("handlebars", handlebarsInstance.engine);
-app.set("view engine", "handlebars");
+const static = express.static(__dirname + '/public');
+app.use('/public', static);
+app.engine('handlebars', handlebarsInstance.engine);
+app.set('view engine', 'handlebars');
 
-app.use("/", (req, res, next) => {
-    console.log(
-        "Req URL",
-        req.url,
-        "Auth Status :",
-        req.session.user ? "User is authenticated" : "User is not authenticated"
-    );
-    next();
+app.use('/', (req, res, next) => {
+	console.log(
+		'Req URL',
+		req.url,
+		'Auth Status :',
+		req.session.user ? 'User is authenticated' : 'User is not authenticated'
+	);
+	next();
 });
 constructorMethod(app);
 
-const PORT = process.env.BACKENDPORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
+	console.log(`Server is running on port: ${PORT}`);
 });
