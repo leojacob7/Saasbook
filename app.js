e3d8a48ffdd4b16d04059cbbca68a67d8f9d0230
Leo B Jacob <leobjacob@gmail.com>
2021-12-11 22:59:00 -0500

    push to saasbook

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use('/', (req, res, next) => {
 constructorMethod(app);
 
 const PORT = 3000;
-app.listen(3001, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log(`Server is running on port: 2002`);
 });
